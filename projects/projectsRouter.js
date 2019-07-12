2ebbcb66d0c40dc5dbd9749bf7e961216f1f07b3
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-12 18:47:04 +0200

    added functionality to update a project and get all actions of a specific project

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -4,6 +4,7 @@ const dbProjects = require('../data/helpers/projectModel.js');
 
 const router = express.Router();
 
+// GET ALL PROJECTS
 router.get('/', async (req, res) => {
   try {
     const projects = await dbProjects.get();
@@ -15,6 +16,30 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET ALL ACTIONS OF PROJECT BY PROJECT ID
+router.get('/:id/actions', async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+
+  try {
+    const projectActions = await dbProjects.getProjectActions(id);
+
+    if (projectActions.length) {
+      res.status(200).json(projectActions);
+    } else {
+      res.status(404).json({
+        message: `The project with the ID: ${id} has no attached actions.`,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: `There was an error getting the actions of the project with the ID: ${id}`,
+    });
+  }
+});
+
+// CREATE NEW PROJECT
 router.post('/', async (req, res) => {
   const {
     body: { name, description },
@@ -34,15 +59,56 @@ router.post('/', async (req, res) => {
   }
 });
 
+// DELETE A PROJECT
 router.delete('/:id', async (req, res) => {
-  /* console.log(req.params); */
+  const {
+    params: { id },
+  } = req;
+
   try {
-    const successFlag = await dbProjects.remove(req.params.id);
+    const successFlag = await dbProjects.remove(id);
     successFlag > 0
-      ? res.status(200).json({ message: 'The project has been removed.' })
-      : res.status(404).json({ error: 'The project could not be found.' });
+      ? res.status(200).json({
+          message: `The project with the ID: ${id} has been removed.`,
+        })
+      : res.status(404).json({
+          error: `The project with the ID: ${id} could not been found.`,
+        });
   } catch (error) {
     res.status(500).json({ error: 'There was an error removing the project.' });
+  }
+});
+
+// UPDATE A PROJECT
+router.put('/:id', async (req, res) => {
+  const {
+    body: { name, description },
+    params: { id },
+  } = req;
+
+  if (name && description) {
+    try {
+      const updatedProject = await dbProjects.update(id, {
+        name,
+        description,
+      });
+
+      if (updatedProject) {
+        res.status(200).json(updatedProject);
+      } else {
+        res.status(204).json({
+          errorMessage: `The project with the ID: ${id} could not been found.`,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        error: `There was an error updating the project with the ID: ${id}.`,
+      });
+    }
+  } else {
+    res.status(404).json({
+      error: 'Could not read required name and or description of the project.',
+    });
   }
 });
 
