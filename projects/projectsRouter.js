a4a812f8b09240df2d5b71a35efeba30bbfdc60b
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-12 18:26:27 +0200

    added project post and delete functionality

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -15,4 +15,35 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  const {
+    body: { name, description },
+  } = req;
+
+  if (!name || !description) {
+    res.status(404).json({
+      error: 'Could not read required name and or description of the project.',
+    });
+  } else {
+    try {
+      const addedProject = await dbProjects.insert(req.body);
+      res.status(201).json(addedProject);
+    } catch (error) {
+      res.status(500).json({ error: 'There was an error adding the project.' });
+    }
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  /* console.log(req.params); */
+  try {
+    const successFlag = await dbProjects.remove(req.params.id);
+    successFlag > 0
+      ? res.status(200).json({ message: 'The project has been removed.' })
+      : res.status(404).json({ error: 'The project could not be found.' });
+  } catch (error) {
+    res.status(500).json({ error: 'There was an error removing the project.' });
+  }
+});
+
 module.exports = router;
